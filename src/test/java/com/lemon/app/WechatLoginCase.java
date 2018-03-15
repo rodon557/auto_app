17de645fc42bb246824969b01171102eb4ad0650
rodon557 <546492510@qq.com>
2018-03-15 13:21:17 +0800

    app

--- src/test/java/com/lemon/app/WechatLoginCase.java
+++ src/test/java/com/lemon/app/WechatLoginCase.java
@@ -1,39 +1,11 @@
 package com.lemon.app;
 
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-public class WechatLoginCase {
-	//deviceName:127.0.0.1:62001
-	private AppiumDriver androidDriver;
-	
-	@BeforeClass
-	public void setUp() throws MalformedURLException{
-		//配置对象，添加deviceName,appPackage,appActivity信息
-		DesiredCapabilities desiredCapabilities =new DesiredCapabilities();
-		desiredCapabilities.setCapability("deviceName", "127.0.0.1:62001");
-		desiredCapabilities.setCapability("appPackage", "com.tencent.mm");
-		desiredCapabilities.setCapability("appActivity", "com.tencent.mm.ui.LauncherUI");
-		//appium接受请求的rest接口
-		URL url=new URL("http://127.0.0.1:4723/wd/hub");
-		//创建驱动对象
-		androidDriver=new AndroidDriver(url,desiredCapabilities);
-	}
+public class WechatLoginCase extends Base {
 	
 	@Test
 	public void test(){
@@ -61,30 +33,6 @@ public class WechatLoginCase {
 		
 		
 	}
-     @AfterClass
-	public void tearDown() {
-		androidDriver.quit();
-		
-	}
-     
-     public WebElement getElement(final By by){
-    	 WebDriverWait wait=new WebDriverWait(androidDriver,30);
-    	 WebElement webElement =wait.until(new ExpectedCondition<WebElement>(){
-    		 public WebElement apply(WebDriver androidDriver){
-    			 return androidDriver.findElement(by);
-    		 }
-    	 });
-    	 return webElement;
-     }
-     
-     public List<WebElement> getElements(final By by){
-    	 WebDriverWait wait=new WebDriverWait(androidDriver,30);
-    	 List<WebElement> webElements =wait.until(new ExpectedCondition<List<WebElement>>(){
-    		 public List<WebElement> apply(WebDriver androidDriver){
-    			 return androidDriver.findElements(by);
-    		 }
-    	 });
-    	 return webElements;
-     }
+   
 
 }
